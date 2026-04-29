a6045cb427b15ccf73371e94dee99ddfc27c0983
HanAMTK <amtkhrc25@gmail.com>
2026-04-29 14:09:40 +0100

    PWA Installed

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,8 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+  VitePWA({
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'CAETS',
+        short_name: 'CAETS',
+        description: 'A centralised academic enquiry ticketing system.',
+        theme_color: '#FFC0CB',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   base: '/KV6027/CAETS/app/',
 })
